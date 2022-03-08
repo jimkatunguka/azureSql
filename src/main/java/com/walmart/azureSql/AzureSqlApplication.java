362182f4b6dc328a4e41bd0c166c1f93c470ca4d
jkatunguka <jkatunguka@aetotechs.com>
2022-03-07 22:47:39 -0500

    initial

---
+++ src/main/java/com/walmart/azureSql/AzureSqlApplication.java
@@ -0,0 +1,13 @@
+package com.walmart.azureSql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzureSqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AzureSqlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/walmart/azureSql/AzureSqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.walmart.azureSql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AzureSqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
